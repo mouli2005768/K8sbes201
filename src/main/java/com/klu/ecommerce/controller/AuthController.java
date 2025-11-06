a21b031376053f4ade5429cdd638e17cc2718a4a
kaka mouli brahma <2300030284@kluniversity.in>
2025-11-06 11:53:50 +0530

    Update AuthController.java

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins="*")
 public class AuthController {
     private final UserService userService;
 
