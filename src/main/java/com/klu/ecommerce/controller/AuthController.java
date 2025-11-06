25138890a5fbd3bee5f8ce1afaf25d807a42f440
kaka mouli brahma <2300030284@kluniversity.in>
2025-11-06 12:02:05 +0530

    Update CORS policy in AuthController
    Restrict CORS to allow requests only from localhost:30082.

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "http://localhost:30082") // 👈 allow frontend requests
 public class AuthController {
     private final UserService userService;
 
